0c7054d1618cb24b779b98c2be9136e8c1db4828
Andrew Adcock <andrewadcock@gmail.com>
2018-03-07 21:41:17 -0500

    Update configurations

--- scripts/wp-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Set up a local wp-config that is outside the web root
-
-cd /var/www/wp-drupalvm/web
-ln -s ../wp-config.php wp-config.php
-cp ../config/wp-config-default.php ../wp-config.php
-
