c1245ca415bcb26b1817e31147ca6b299e46ef96
Andrew Adcock <andrewadcock@gmail.com>
2018-03-07 23:34:23 -0500

    Update WordPress version, adjust config and setup script

--- scripts/wp-drupalvm-setup.sh
+++ scripts/wp-drupalvm-setup.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-# Set up a local wp-config that is outside the web root
 
+
+## Update Nginx configs with defaults
+echo "Start Nginx Rewrites zzzzz"
+cd /var/www/wp-drupalvm/config/defaults/nginx
+cp _php_fastcgi.conf /etc/nginx/
+rm /etc/nginx/sites-enabled/wp-drupalvm.test.conf
+cp wp-drupalvm.test.conf /etc/nginx/sites-enabled/wp-drupalvm.test.conf
+/etc/init.d/nginx restart
+
+# Set up a local wp-config that is outside the web root
 cd /var/www/wp-drupalvm/web
 ln -s ../wp-config.php wp-config.php
 cp ../config/wp-config-default.php ../wp-config.php
 
+
+touch /etc/nginx/sites-enabled/test.test
